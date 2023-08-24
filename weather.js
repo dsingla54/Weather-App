a372bf96d652b91e4b0f139ae061180658fed8fd
Dhruv Singla <139515749+dsingla54@users.noreply.github.com>
2023-08-24 16:43:01 +0530

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,27 @@
+const apiKey = '21611f0f96958a0195b5ebda9061cc55'; // Replace with your API key
+
+document.getElementById('getWeather').addEventListener('click', getWeather);
+
+function getWeather() {
+    const city = document.getElementById('cityInput').value;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            displayWeather(data);
+        })
+        .catch(error => {
+            console.error('Error fetching weather data:', error);
+        });
+}
+
+function displayWeather(data) {
+    const weatherInfo = document.getElementById('weatherInfo');
+    weatherInfo.innerHTML = `
+        <h2>Weather in ${data.name}, ${data.sys.country}</h2>
+        <p>Temperature: ${data.main.temp}°C</p>
+        <p>Weather: ${data.weather[0].main}</p>
+        <p>Description: ${data.weather[0].description}</p>
+    `;
+}
